6c8b0b2bfb758ededbab931291838320009139a9
billhuang1 <bill.huang@atos.net>
2025-06-27 20:00:56 +0800

    no message

---
+++ src/main/java/com/tsb/LogaspectApplication.java
@@ -0,0 +1,13 @@
+package com.tsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogaspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogaspectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsb/LogaspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogaspectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
